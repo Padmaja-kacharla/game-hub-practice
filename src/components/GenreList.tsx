50fd998c89dfcd64e7fa6992b046bada9b6b29dc
Padmaja-kacharla <padmajas9821@gmail.com>
2024-10-01 01:32:17 -0700

    Displayimg Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import UseGenre from "../hooks/UseGenre";
+import getCroppedImage from "../Services/image-url";
 
 const GenreList = () => {
   const { data } = UseGenre();
   return (
     <div>
-      {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImage(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 };
